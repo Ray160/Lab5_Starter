72b99d5f341de5215314f1dc62a7098a23ebefa4
Ray160 <41882725+Ray160@users.noreply.github.com>
2021-10-21 15:50:39 -0700

    init voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,6 +4,22 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  let voices = SpeechSynthesis.getVoices();
-  document.getElementById("voice-select");
+  let synth = window.speechSynthesis;
+  let voices = synth.getVoices();
+
+  let voiceSelect = document.getElementById("voice-select");
+  console.log(voices);
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.add(option);
+  }
+  
 }
