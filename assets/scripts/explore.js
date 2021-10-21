7ee5961ffb92215d3de5812fe3672f231a335653
Ray160 <41882725+Ray160@users.noreply.github.com>
2021-10-21 14:29:34 -0700

    1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,6 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let voices = SpeechSynthesis.getVoices();
+  document.getElementById("voice-select");
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,7 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let image = document.getElementsByTagName('img');
+
+  image.src = 'assets/images/air-horn.svg';
 }
